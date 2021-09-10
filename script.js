b25ba0042359c78d7962744386b013896be46b83
PinkSpaceBee <minaary@mailfence.com>
2021-09-10 11:42:16 +0300

    add basic arithmetic operations

---
+++ script.js
@@ -0,0 +1,18 @@
+function add (a,b) {
+    let result = (a + b).toFixed(1);
+    return result;
+}
+function substract (a,b) {
+    let result = (a - b).toFixed(1);
+    return result;
+}
+function multiply (a,b) {
+    let result = (a * b).toFixed(1);
+    return result;
+}
+function divide (a,b) {
+    let result = (a / b).toFixed(1);
+    return result;
+}
+
+
